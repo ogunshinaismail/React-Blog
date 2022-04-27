fab482bed5631e0d7f32561db3f57f1d697936f1
ismail <ismailogunshina@gmail.com>
2022-04-27 21:12:07 +0100

    blog commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import './App.css';
-import Form from './Form';
-import Forms from './Forms';
+// import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
+
   return (
-    <div>
-      <Forms />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Bloglist.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Bloglist = ({ blogs, title }) => {
+    
+    return (
+        <div className='blogCard'>
+            <h1>{title}</h1>
+
+            {blogs.map(blog => (
+                <div key={blog.id} className='blog-preview'>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>{blog.author}</p>
+                    </Link>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Bloglist
