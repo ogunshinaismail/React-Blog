98ba707a124a94641e4fafee48ea892d75265c70
Ogunshina ismail <ismailogunshina@gmail.com>
2022-08-09 22:34:48 -0700

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   return (
     <nav className="navbar">
-        <h1>The Dojo Blog</h1>
+        <h1>React Blog</h1>
         <div className="links">
             <Link to="/">Home</Link>
             <Link to="/create">New Blog</Link>
